6e6c3f1a6fe0501fb74e494305224cdceedee9ce
x8848 <8848484@gmail.com>
2024-01-24 13:19:13 +0100

    drop box fix

--- components/Droppable.tsx
+++ components/Droppable.tsx
@@ -6,13 +6,8 @@ import { FC } from 'react'
 const Droppable: FC<DroppableProps> = ({ id, children, className, onDrop }) => {
   const drop = async (e: React.DragEvent<HTMLDivElement>) => {
     e.preventDefault()
-    const targetId = (e.target as EventTarget & { id: string }).id
     const objectId = e.dataTransfer.getData('id')
-    if (objectId && targetId) {
-      await onDrop(objectId, targetId)
-    } else {
-      alert(`Can't drop to this area`)
-    }
+    if (objectId) await onDrop(objectId, id)
   }
 
   const allowDrop = (e: React.DragEvent<HTMLDivElement>) => e.preventDefault()
